5136bc4f40617556dc1b5be3d1749e1d2be0482a
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-18 13:41:05 +0200

    feat(Doc Homepage): add homepage route

--- src/Router/routes.php
+++ src/Router/routes.php
@@ -6,7 +6,12 @@
  */
 
 
+use MvcLite\Controllers\Documentation\HomeController;
 use MvcLite\Controllers\IndexController;
 use MvcliteCore\Router\Router;
 
-Router::get("/", IndexController::class, "render");
+Router::get("/", IndexController::class, "render")
+    ->setName("home");
+
+Router::get("/docs/home", HomeController::class, "render")
+    ->setName("docs.home");
